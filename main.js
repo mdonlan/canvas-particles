196256612091f9f923f56219d8d5928b7692ce25
mdonlan <mdonlan8792@yahoo.com>
2018-05-04 21:19:43 -0400

    working on control panel

--- main.js
+++ main.js
@@ -4,19 +4,27 @@ let canvas = document.querySelector(".canvas");
 let ctx = canvas.getContext('2d');
 let canvasHeight = canvas.height;
 let canvasWidth = canvas.width;
-let pointWidth = 3;
+let pointRadius = 3;
 let frames = 0;
+let particleSpeed = 3;
+let oldParticleSpeed = null;
 let startTime = null;
 let endTime = null;
+let connectionDistance = 100;
+let oldConnectionDistance = null;
+
+// set element refs
 let fpsCounter = document.querySelector(".fpsCounter");
+let particleSpeedElem = document.querySelector(".particleSpeed");
+let connectionDistanceElem = document.querySelector(".connectionDistance");
 
 function createPoints() {
   let numPoints = 20;
   for(let i = 0; i < numPoints; i++) {
     let x = Math.floor(Math.random() * canvasWidth) + 1;
     let y = Math.floor(Math.random() * canvasHeight) + 1;
-    let velX = (Math.floor(Math.random() * 10) + 1) / 50;
-    let velY = (Math.floor(Math.random() * 10) + 1) / 50;
+    let velX = ((Math.floor(Math.random() * 10) - 5) / 50);
+    let velY = ((Math.floor(Math.random() * 10) - 5) / 50);
     let color = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 
     let point = {
@@ -31,26 +39,26 @@ function createPoints() {
 };
 
 function movePoint(point) {
-  point.x+=point.velX;
-  point.y+=point.velY;
+  point.x+=point.velX * particleSpeed;
+  point.y+=point.velY * particleSpeed;
 
   // check if new location will be inside canvas
   checkIfValidPosition(point)
 }
 
 function checkIfValidPosition(point) {
-  if(point.x <= 0 || point.x + pointWidth >= canvas.width) {
+  if(point.x - pointRadius <= 0 || point.x + pointRadius >= canvas.width) {
     point.velX = -point.velX;
   }
 
-  if(point.y <= 0 || point.y + pointWidth >= canvas.height) {
+  if(point.y - pointRadius <= 0 || point.y + pointRadius >= canvas.height) {
     point.velY = -point.velY;
   }
 }
 
 function drawPoint(point) {
   ctx.beginPath();
-  ctx.arc(point.x, point.y, pointWidth, 0, 2*Math.PI);
+  ctx.arc(point.x, point.y, pointRadius, 0, 2*Math.PI);
   ctx.fillStyle = point.color;
   ctx.strokeStyle = point.color;
   ctx.fill();
@@ -71,9 +79,49 @@ function update() {
     movePoint(points[i]);
     drawPoint(points[i]);
     findConnections(points[i]);
+    updateUI();
   }
   getFPS();
   window.requestAnimationFrame(update);
+};
+
+function changedUI(event) {
+  // responds to user clicks that change UI data
+
+  if(event.target.classList[0] == 'increaseParticleSpeed') {
+    console.log('increasing speed')
+    particleSpeed++;
+  } else if(event.target.classList[0] == 'decreaseParticleSpeed') {
+    console.log('decreasing speed')
+    particleSpeed--;
+  } 
+
+  if(event.target.classList[0] == 'increaseConnectionDistance') {
+    console.log('increasing connection dist')
+    connectionDistance = connectionDistance + 10;
+  } else if(event.target.classList[0] == 'decreaseConnectionDistance') {
+    console.log('decreasing connection dist')
+    connectionDistance = connectionDistance - 10;
+  } 
+
+}
+
+function updateUI() {
+  // runs in update, detects and changes to 
+  // data that is in UI and updates
+  if(oldParticleSpeed != particleSpeed) {
+    // if particle speed setting has changed
+    particleSpeedElem.innerHTML = particleSpeed;
+    oldParticleSpeed = particleSpeed;
+    console.log('updating particle speed')
+  }
+
+  if(oldConnectionDistance != connectionDistance) {
+    // if particle speed setting has changed
+    connectionDistanceElem.innerHTML = connectionDistance;
+    oldConnectionDistance = connectionDistance;
+    console.log('updating connection distance speed')
+  }
 };
 
 function getFPS() {
@@ -107,7 +155,7 @@ function clear() {
 function findConnections(point) {
   for(var i = 0; i < points.length; i++) {
     let distance = Math.hypot(point.x - points[i].x, point.y - points[i].y);
-    if(distance < 50 && points[i] != point) {
+    if(distance < connectionDistance && points[i] != point) {
       if(points[i] === point) {
         console.log('testing')
       }
